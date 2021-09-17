577d5a0d1c7f507378f132f6e98086096fe4f5b5
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2021-09-17 11:02:53 +0800

    Formatted

--- EZBook/ezbook_project/models.py
+++ EZBook/ezbook_project/models.py
@@ -2,68 +2,62 @@ from django.db import models
 
 # Create your models here.
 
+
 class User(models.Model):
-    user_pic = models.CharField(max_length=100,default='',null=False)
-    username = models.CharField(max_length=100,default='',null=False)
-    password = models.CharField(max_length=100,default='',null=False)
-    firstname = models.CharField(max_length=100,default='',null=False)
-    middlename = models.CharField(max_length=100,default='',null=False)
-    lastname = models.CharField(max_length=100,default='',null=False)
+    user_pic = models.CharField(max_length=100, default='', null=False)
+    username = models.CharField(max_length=100, default='', null=False)
+    password = models.CharField(max_length=100, default='', null=False)
+    firstname = models.CharField(max_length=100, default='', null=False)
+    middlename = models.CharField(max_length=100, default='', null=False)
+    lastname = models.CharField(max_length=100, default='', null=False)
     age = models.IntegerField()
-    phone_number = models.CharField(max_length=100,default='',null=False)
+    phone_number = models.CharField(max_length=100, default='', null=False)
     email = models.EmailField()
     date_created = models.DateField()
+
 
 class Company(models.Model):
-    username = models.CharField(max_length=100,default='',null=False)
-    password = models.CharField(max_length=100,default='',null=False)
-    name_company = models.CharField(max_length=100,default='',null=False)
+    username = models.CharField(max_length=100, default='', null=False)
+    password = models.CharField(max_length=100, default='', null=False)
+    name_company = models.CharField(max_length=100, default='', null=False)
     date_created = models.DateField()
     email = models.EmailField()
-    phone_number = models.CharField(max_length=100,default='',null=False)
-    company_pic = models.CharField(max_length=100,default='',null=False)
-     
+    phone_number = models.CharField(max_length=100, default='', null=False)
+    company_pic = models.CharField(max_length=100, default='', null=False)
+
 
 class Driver(models.Model):
-    driver_pic = models.CharField(max_length=100,default='',null=False)
-    driver_fullname = models.CharField(max_length=100,default='',null=False)
-    company_name = models.ForeignKey(Company, on_delete = models.CASCADE)
-
+    driver_pic = models.CharField(max_length=100, default='', null=False)
+    driver_fullname = models.CharField(max_length=100, default='', null=False)
+    company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
 class Vehicle(models.Model):
-    company_id = models.ForeignKey(Company, on_delete = models.CASCADE)
-    vehicle_plate = models.CharField(max_length=100,default='',null=False)
-    vehicle_driver_id =models.ForeignKey(Driver, on_delete = models.CASCADE)
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
+    vehicle_plate = models.CharField(max_length=100, default='', null=False)
+    vehicle_driver_id = models.ForeignKey(Driver, on_delete=models.CASCADE)
     vehicle_capacity = models.IntegerField()
-    vehicle_type = models.CharField(max_length=100,default='',null=False)
-    vehicle_pic = models.CharField(max_length=100,default='',null=False)
+    vehicle_type = models.CharField(max_length=100, default='', null=False)
+    vehicle_pic = models.CharField(max_length=100, default='', null=False)
+
 
 class Travel_Places(models.Model):
-    places = models.CharField(max_length=100,default='',null=False)
-
+    places = models.CharField(max_length=100, default='', null=False)
 
 
 class BookTravel(models.Model):
-    vehicle_id = models.ForeignKey(Vehicle, on_delete = models.CASCADE)
-    place_from = models.ForeignKey(Travel_Places, on_delete = models.CASCADE,related_name='places_from')
-    place_to = models.ForeignKey(Travel_Places, on_delete = models.CASCADE,related_name='places_to')
+    vehicle_id = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    place_from = models.ForeignKey(
+        Travel_Places, on_delete=models.CASCADE, related_name='places_from')
+    place_to = models.ForeignKey(
+        Travel_Places, on_delete=models.CASCADE, related_name='places_to')
     available_travel_date = models.DateField()
     available_travel_time = models.TimeField()
-    company = models.ForeignKey(Company, on_delete = models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
 
 class Booking(models.Model):
     booking_date = models.DateField()
     seat_number = models.IntegerField()
-    user_travel_id = models.ForeignKey(User, on_delete = models.CASCADE)
-    book_travel_id = models.ForeignKey(BookTravel, on_delete = models.CASCADE)
-
-    
-
-
-
-
-
-
-
+    user_travel_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    book_travel_id = models.ForeignKey(BookTravel, on_delete=models.CASCADE)
--- EZBook/ezbook_project/views.py
+++ EZBook/ezbook_project/views.py
@@ -1,49 +1,69 @@
 from django.shortcuts import render
 from django.views.generic import View
-from ezbook_project.models import User, Company, Driver, Vehicle,Travel_Places,BookTravel,Booking
+from ezbook_project.models import User, Company, Driver, Vehicle, Travel_Places, BookTravel, Booking
 
 # Create your views here.
 
+
 class StudentIndexView(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'home.html')
 
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'home.html')
+
+
 def about(request):
-   return render(request,'about.html',{'title':'about'})
+    return render(request, 'about.html', {'title': 'about'})
+
+
 def features(request):
-     return render(request,'features.html',{'title':'features'})
+    return render(request, 'features.html', {'title': 'features'})
+
+
 def signin(request):
-    return render(request,'signin.html',{'title':'signin'})
+    return render(request, 'signin.html', {'title': 'signin'})
+
+
 def register(request):
-    return render(request,'register.html',{'title':'register'})
+    return render(request, 'register.html', {'title': 'register'})
+
+
 def contact(request):
-    return render(request,'contact.html',{'title':'contact'})
+    return render(request, 'contact.html', {'title': 'contact'})
+
 
 def dashboard(request):
     users = User.objects.all()
-    return render(request,'dashboard.html',{'users':users})
+    return render(request, 'dashboard.html', {'users': users})
+
+
 def dashboard_company(request):
     companies = Company.objects.all()
-    return render(request,'dashboard_companies.html',{'companies':companies})
+    return render(request, 'dashboard_companies.html', {'companies': companies})
+
 
 def dashboard_driver(request):
     drivers = Driver.objects.all()
-    return render(request,'dashboard_drivers.html',{'drivers':drivers})
+    return render(request, 'dashboard_drivers.html', {'drivers': drivers})
+
 
 def dashboard_vehicle(request):
     vehicles = Vehicle.objects.all()
-    return render(request,'dashboard_vehicle.html',{'vehicles':vehicles})
+    return render(request, 'dashboard_vehicle.html', {'vehicles': vehicles})
+
 
 def dashboard_travelplaces(request):
     travel_places = Travel_Places.objects.all()
-    return render(request,'dashboard_travelplaces.html',{'travel_places':travel_places})
+    return render(request, 'dashboard_travelplaces.html', {'travel_places': travel_places})
+
 
 def dashboard_booktravel(request):
     available_bookings = BookTravel.objects.all()
-    return render(request,'dashboard_availablebookings.html',{'available_bookings':available_bookings})
+    return render(request, 'dashboard_availablebookings.html', {'available_bookings': available_bookings})
+
 
 def dashboard_booking(request):
     bookings = Booking.objects.all()
-    return render(request,'dashboard_bookings.html',{'bookings':bookings})
+    return render(request, 'dashboard_bookings.html', {'bookings': bookings})
